7f799cfa50353d5222b7324bb682ae46baf5912a
Flemm <flemm.19@gmail.com>
2016-06-29 11:55:46 -0300

    WIP FileSharing

--- app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
+++ app/src/main/java/com/example/whatever/test/androiddemos/MainActivity.java
@@ -9,5 +9,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textview = (TextView) findViewById(R.id.txv_filepath);
+        try {
+            textview.setText(getFilesDir().getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param folderName O nome da pasta local compartilhada
+     * @param fileName O nome do arquivo a ser compartilhado
+     * @return A uri do arquivo, que pode ser passada para outro app via Intent
+     * */
+    private Uri getSharedFileUri(String folderName, String fileName){
+        File imagePath = new File(getFilesDir(), folderName);
+        File newFile = new File(imagePath, fileName);
+        Uri contentUri = FileProvider.getUriForFile(this, "br.org.reconcavo.fileprovider", newFile);
+        return contentUri;
     }
 }
